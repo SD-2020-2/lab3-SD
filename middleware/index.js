e28da0e3a68b1a800d374abf0c03138af9be8fd4
augusticor <rojascruzoscar@gmail.com>
2021-03-08 16:35:02 -0500

    V funcionando creacion de instancias

--- middleware/index.js
+++ middleware/index.js
@@ -1,47 +1,13 @@
 const express = require('express');
 const app = express();
-const exec = require('child_process').exec;
-const port = 2000;
+const port = 3000;
+const bodyParser = require('body-parser');
 
-setInterval(() => {
-	exec('bash watchmv.sh', (err, stdout, stderr) => {
-		if (err) {
-			console.error(`exec error: ${err}`);
-			return;
-		} else {
-			console.log('archivo creado jaja');
-		}
-	});
-	readLastLines.read('asd.txt', 20).then((lines) => {
-		let data = lines.split('\n');
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].includes('Servidor')) {
-				if (data[i + 1].includes('Funcionando') == false) {
-					serverAStatus = 'FAIL';
-					listaServidoresStatus[counter] = serverAStatus;
-					counter++;
-					i++;
-				} else {
-					serverAStatus = 'OK';
-					listaServidoresStatus[counter] = serverAStatus;
-					counter++;
-					i++;
-				}
-			}
-			var counAux = 0;
-			for (let i = 0; i < listaServidores.length; i++) {
-				if (listaServidores[i] != undefined) {
-					counAux++;
-				}
-			}
-			if (counter >= counAux) {
-				counter = 0;
-			}
-			showListaServer();
-		}
-	});
-}, 5000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+app.use(require('./routes/routes'));
+app.use(express.static('public'));
 
 app.listen(port, () => {
 	console.log(`Middleware listening on port: ${port}`);
--- middleware/watchmv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm asd.txt
-rm dfg.txt
-"(date +TIME:%H:%M:%S;echo Servidor1;curl --connect-timeout 5 192.168.0.9:3000; echo '';) >> asd.txt"
