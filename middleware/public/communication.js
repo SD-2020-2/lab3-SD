64affa3b96c277801e8b593d90366788a61b7740
augusticor <rojascruzoscar@gmail.com>
2021-03-11 08:17:56 -0500

    Funcionando con ui mejorada
    - Lista usuarios y los muestra en la tabla
    - Crear usuarios en la bd

--- middleware/public/communication.js
+++ middleware/public/communication.js
@@ -1,38 +1,41 @@
-const name_user = document.querySelector("#name");
-const id_user = document.querySelector("#user_id");
+const name_user = document.querySelector('#name');
+const id_user = document.querySelector('#user_id');
 
 //
 const createUser = function () {
-  console.log("create");
-  let userName = { id: id_user.value, name: name_user.value };
-  let options = {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify(userName),
-  };
+	console.log('create');
+	let userName = { id: id_user.value, name: name_user.value };
+	let options = {
+		method: 'POST',
+		headers: { 'Content-type': 'application/json' },
+		body: JSON.stringify(userName),
+	};
 
-  fetch("/users", options)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+	fetch('/users', options)
+		.then((response) => response.json())
+		.then((data) => console.log(data));
 };
 
 // obtiene la lista de usuarios
 
-function getList() {
-  fetch("/users")
-    .then((response) => response.json())
-    .then((obj) => (servidores.userList = obj))
-    .catch((error) => console.log(error));
-}
-
 var servidores = new Vue({
-  el: "#userinfo",
-  data() {
-    return {
-      userList: [],
-    };
-  },
+	el: '#userinfo',
+	data() {
+		return {
+			userList: [],
+		};
+	},
 });
+
+function getList() {
+	fetch('/users')
+		.then((response) => response.json())
+		.then((obj) => {
+			servidores.userList = obj;
+			console.log(obj);
+		})
+		.catch((error) => console.log(error));
+}
 
 // const btnAdd = document.querySelector('.btn_Add');
 // btnAdd.addEventListener('click', (event) => {
@@ -40,10 +43,11 @@ var servidores = new Vue({
 // 	createUser();
 // });
 
-const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  createUser();
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	createUser();
 });
-const buttonGet = document.querySelector(".btn_getList");
-buttonGet.addEventListener("click", getList);
+
+const buttonGet = document.querySelector('.btn_getList');
+buttonGet.addEventListener('click', getList);
